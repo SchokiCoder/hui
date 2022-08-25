2af377b3800ca1f7ceca215e0f664511d01d0523
SchokiCoder <afschoknecht@gmail.com>
2022-08-25 11:52:35 +0200

    added config reading via json file

---
+++ src/config.rs
@@ -0,0 +1,137 @@
+/*
+	"House Desktop Environment" in short "house_de"
+	Copyright (C) 2022	Andy Frank Schoknecht
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use std::vec::Vec;
+use std::string::String;
+use std::fs::File;
+use std::io::Read;
+use json::JsonValue;
+
+pub struct Menu {
+	label: String,
+	buttons: Vec<Button>,
+}
+
+impl Menu {
+	pub fn from_json(mut json: JsonValue) -> Result<Menu, &'static str> {
+		let mut result = Menu {
+			label: json["label"].to_string(),
+			buttons: Vec::<Button>::new(),
+		};
+		
+		loop {
+			let button = json["buttons"].array_remove(0);
+			if button == JsonValue::Null {
+				break;
+			}
+			
+			let btn_result = Button::from_json(button);
+			
+			if !btn_result.is_ok() {
+				return Err("Button in menu corrupt");
+			}
+			
+			result.buttons.push(btn_result.unwrap());
+		}
+		
+		return Ok(result);
+	}
+}
+
+pub struct Button {
+	label: String,
+	buttons: Vec<Button>,
+	cmd: String,
+}
+
+impl Button {
+	pub fn from_json(mut json: JsonValue) -> Result<Button, &'static str> {
+		let mut result = Button {
+			label: json["label"].to_string(),
+			buttons: Vec::<Button>::new(),
+			cmd: json["cmd"].to_string(),
+		};
+		
+		loop {
+			let button = json["buttons"].array_remove(0);
+			if button == JsonValue::Null {
+				break;
+			}
+			
+			let btn_result = Button::from_json(button);
+			
+			if !btn_result.is_ok() {
+				return Err("Button in submenu corrupt");
+			}
+			
+			result.buttons.push(btn_result.unwrap());
+		}
+		
+		return Ok(result);
+	}
+}
+
+pub struct UserConfig {
+	motd: String,
+	main_menu: Menu,
+}
+
+impl UserConfig {
+	pub fn from_json(filepath: &str) -> Result<UserConfig, &'static str> {	
+		let mut result = UserConfig {
+			motd: String::new(),
+			main_menu: Menu {
+				label: String::new(),
+				buttons: Vec::<Button>::new(),
+			},
+		};
+	
+		// open etc file
+		let etcpath = String::from(filepath);
+		
+		let f = File::open(etcpath.as_str());
+		
+		if !f.is_ok() {
+			return Err("User config could not be opened")
+		}
+		
+		let mut f = f.unwrap();
+		
+		// read file
+		let mut text = String::new();
+		if !f.read_to_string(&mut text).is_ok() {
+			return Err("User config could not be read")
+		}
+		
+		// parse json
+		let mut json = json::parse(text.as_str()).unwrap();
+		
+		// convert to usable config		
+		result.motd = json["motd"].to_string();
+		
+		let mnu_result = Menu::from_json(json["main_menu"].take());
+		
+		if !mnu_result.is_ok() {
+			return Err("Main menu is corrupt");
+		}
+		
+		result.main_menu = mnu_result.unwrap();
+
+		return Ok(result);
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -16,135 +16,15 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use std::collections::HashMap;
-use std::vec::Vec;
-use std::string::String;
-use std::fs::File;
-use std::io::Read;
-use serde_derive::Deserialize;
-
-struct Command {
-	label: String,
-	command: String,
-}
-
-struct Menu {
-	label: String,
-	buttons: Vec<Button>,
-}
-
-enum Button {
-	Command(Command),
-	Menu(Menu),
-}
-
-struct UserConfig {
-	main_menu: Menu,
-}
-
-impl UserConfig {
-	pub fn new(username: &str) -> Result<UserConfig, String> {
-		#[derive(Deserialize)]
-		struct ButtonRaw {
-			label: String,
-			menu: Vec<ButtonRaw>,
-			command: String,
-		}
-		
-		#[derive(Deserialize)]
-		struct MenuRaw {
-			label: String,
-			buttons: Vec<ButtonRaw>,
-		}
-		
-		#[derive(Deserialize)]
-		struct UserConfigRaw {
-			motd: String,
-			main_menu: MenuRaw,
-		}
-		
-		let mut result = UserConfig {
-			main_menu: Menu {
-				label: String::from("Main"),
-				buttons: Vec::<Button>::new(),
-			},
-		};
-	
-		// open etc file
-		/*let mut etcpath = String::from("/etc/");
-		etcpath.push_str(env!("CARGO_PKG_NAME"));
-		etcpath.push_str(".d/");
-		etcpath.push_str(username);*/
-		let etcpath = String::from(username); // temp override
-		
-		let f = File::open(etcpath.as_str());
-		
-		if !f.is_ok() {
-			return Err(format!("User config \"{}\" could not be opened", etcpath))
-		}
-		
-		let mut f = f.unwrap();
-		
-		// read file
-		let mut text = String::new();
-		if !f.read_to_string(&mut text).is_ok() {
-			return Err(format!("User config \"{}\" could not be read", etcpath))
-		}
-		
-		// parse toml
-		let rawcfg: UserConfigRaw = toml::from_str(text.as_str()).unwrap();
-		
-		// convert to usable config
-		
-/*
-		// read lines
-		let mut i: usize = 0;
-		let lines: Vec<&str> = text.split('\n').collect();
-		
-		while i < lines.len() {
-			// read words
-			let words: Vec<&str> = lines[i].trim().split(' ').collect();
-			
-			match words[0] {
-				"buttons" => {
-					i += 1;
-					
-					while i < lines.len() {
-						// count indentation
-						let mut indent: usize = 0;
-						
-						for c in lines[i].chars() {
-							if c == '\t' {
-								indent += 1;
-							}
-							else {
-								break;
-							}
-						}
-						
-						// if indent ended, stop
-						if indent <= 0 {
-							break;
-						}
-						
-						// read words
-						
-						
-						 i += 1;
-					}
-				},
-				
-				_ => (),
-			}
-			
-			i += 1;
-		}
-*/
-
-		return Ok(result);
-	}
-}
+mod config;
+use config::UserConfig;
 
 fn main() {
-	let _usercfg = UserConfig::new("example_etc/house_de.d/generic_guard.toml");
+	let etcpath = String::from("example_etc/house_de.d/generic_guard.json");/*let mut etcpath = String::from("/etc/");
+	etcpath.push_str(env!("CARGO_PKG_NAME"));
+	etcpath.push_str(".d/");
+	etcpath.push_str(username);*/
+	// temp override
+	
+	let _usercfg = UserConfig::from_json(etcpath.as_str());
 }
